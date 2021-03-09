6c4d9c2ee8c68a4c437f57b7a8ce001f67ce214f
Michael Alex Lynch <mlynch5187@gmail.com>
2021-03-08 17:58:27 -0700

    Safety bar lowers when lfit is full

--- src/Lift.js
+++ src/Lift.js
@@ -13,14 +13,18 @@ class Lift  {
   }
 
   admitSkier(name, ticket_status)  {
-    if (this.skiers.length < 2 && ticket_status === true)  {
+    if (this.skiers.length < this.limit && ticket_status === true)  {
       var skier = new Skier(name, ticket_status)
       this.skiers.push(skier)
-    } else if (this.skiers.length >= 2) {
+    } else if (this.skiers.length >= this.limit) {
       return `Sorry, ${name}. Please wait for the next lift!`
     } else if (ticket_status === false) {
       return `Sorry, ${name}. You need a lift ticket!`
     }
+  }
+
+  startLift() {
+    this.safetyBar = 'down';
   }
 }
 
